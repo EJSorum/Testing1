c57cc39d8d591686ec8bfe3836ef5f665dedb28a
Eric Sorum <ejsorum@gmail.com>
2022-04-27 07:36:36 -0500

    Cleaned up code.

--- src/cipher.js
+++ src/cipher.js
@@ -2,25 +2,9 @@
 cipher() takes two parameters: an integer and a string.  
 We find the UFC-16 character code for each character in the string, and increase it
 by the amount of the integer.  We put these new characters into a new string.
+We take care to keep the cipher case-sensitive.  
 
-
-First we define the wrap function, which takes the character code and wraps it
-back to the beginning of the alphabet if the cipher makes it go past Z in the alphabet.
 */
-
-/*
-function wrap(charCode) {
-    if (charCode > 122) {
-        return 97 + charCode - 123
-    } if (charCode > 64 && charCode < 98) {
-       return 65 + charCode - 99
-    } 
-        return charCode   
-}
-*/
-
-// CORRECT VERSION BELOW???
-// A = 65, Z=90, a = 97, z=122
 function cipher(int, str) {
   let newStr = "";
   for (let i = 0; i < str.length; i += 1) {
@@ -41,24 +25,5 @@ function cipher(int, str) {
   }
   return newStr;
 }
-
-/*
-function cipher(int, str) {
-  let newStr = "";
-  for (let i = 0; i < str.length; i += 1) {
-    if (/[a-z]/i.test(str[i])) {
-      const code = str.charCodeAt[i];
-      if (code > 96) {
-        newStr = newStr.concat(String.fromCharCode(wrap(code + (int % 26))));
-      } else {
-        newStr = newStr.concat(String.fromCharCode(wrap(65 + (int % 26))));
-      }
-    } else {
-      newStr = newStr.concat(str[i]);
-    }
-  }
-  return newStr;
-}
-*/
 
 module.exports = cipher;
--- src/cipher.test.js
+++ src/cipher.test.js
@@ -2,6 +2,9 @@ const cipher = require("./cipher");
 
 test("if the cipher shifts each letter", () => {
   expect(cipher(1, "abcde")).toMatch(/bcdef/);
+  // This is a basic test to ensure the cipher shifts the letters over one place in the alphabet.
   expect(cipher(53, "abcde")).toMatch(/bcdef/);
+  // This test ensures the cipher can handle large integers by looping back to the beginning of the alphabet.
   expect(cipher(1, "Z;Z Z!")).toMatch(/A;A A!/);
+  // This test esnures the cipher keeps the letters case-sensitive and leaves spaces and punctuation unchanged.
 });
